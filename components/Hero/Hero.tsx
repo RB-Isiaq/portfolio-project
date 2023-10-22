78be5090556a6573b117c59a093bef4147494e27
rb-isiaq <isiaqridwanbukola1999@gmail.com>
2023-10-22 07:32:15 +0100

    added typewritter effect

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,8 +1,64 @@
+// import Typewriter from 'typewriter-effect';
+// import Image from 'next/image';
+// import styles from './Hero.module.scss';
+// import Profile from '@/public/myself-bg.png';
+// import { TestId } from './constants';
+
+// const Hero = () => {
+//   return (
+//     <section id="home" className={styles.hero} data-testid={TestId.HERO_ID}>
+//       <div className={styles.details}>
+//         <h1 className={styles.name} data-testid={TestId.HERO_NAME}>
+//           Ridwan Bukola <span>Isiaq</span>
+//           <Typewriter
+//             options={{
+//               strings: ['Ridwan', 'Bukola', 'Isiaq'],
+//               autoStart: true,
+//               loop: true,
+//             }}
+//           />
+//         </h1>
+//         <div className={styles.profession}>
+//           <h1>Frontend Developer</h1>
+//           <h2>ReactJS | NextJS | Typescript</h2>
+//         </div>
+//         <a
+//           className={styles.btn}
+//           rel="noopener"
+//           href="/Resume.pdf"
+//           download="Ridwan's Resume"
+//           target="_blank"
+//           data-testid={TestId.RESUME}
+//         >
+//           Résumé
+//         </a>
+//       </div>
+//       <div className={styles.image}>
+//         <Image
+//           src={Profile}
+//           alt="Isiaq"
+//           className={styles.img}
+//           width={432}
+//           height={577}
+//           data-testid={TestId.HERO_IMG}
+//         />
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Hero;
+'use client';
+
 import Image from 'next/image';
 import styles from './Hero.module.scss';
 import Profile from '@/public/myself-bg.png';
 import { TestId } from './constants';
+import dynamic from 'next/dynamic';
 
+const TypewriterComponent = dynamic(() => import('typewriter-effect'), {
+  ssr: false,
+});
 const Hero = () => {
   return (
     <section id="home" className={styles.hero} data-testid={TestId.HERO_ID}>
@@ -11,7 +67,19 @@ const Hero = () => {
           Ridwan Bukola <span>Isiaq</span>
         </h1>
         <div className={styles.profession}>
-          <h1>Frontend Developer</h1>
+          <TypewriterComponent
+            component="h1"
+            options={{
+              autoStart: true,
+              loop: true,
+              strings: ['Frontend Developer', 'Software Developer'],
+              delay: 150,
+              skipAddStyles: true,
+              cursor: '🖋️',
+              deleteSpeed: 100,
+              cursorClassName: 'cursor',
+            }}
+          />
           <h2>ReactJS | NextJS | Typescript</h2>
         </div>
         <a
